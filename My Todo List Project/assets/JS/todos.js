a48a5d095be832eabfc981306e6c5f8690d4b719
Raluca C <32771192+RalucaCi@users.noreply.github.com>
2019-09-25 22:39:22 +0200

    V1 -  TodoList

---
+++ My Todo List Project/assets/JS/todos.js
@@ -0,0 +1,31 @@
+// check off specific todos by clicking
+$('ul').on('click', 'li', function() {
+	$(this).toggleClass('done');
+});
+
+// click on x to delete todo
+$('ul').on('click', 'span', function(event) {
+	event.stopPropagation();
+	// first fadeout then remove:
+	$(this).parent().fadeOut(500, function() {
+		$(this).remove();
+	});
+});
+
+// Creating a Todo:
+$("input[type='text']").keypress(function(event) {
+	// this will listen for the ENTER key - nr 13:
+	if (event.which === 13) {
+		// grab new todo text from input
+		var todoText = $(this).val();
+		// create a new li and add to ul
+		$('ul').append('<li><span><i class="fas fa-trash"></i></span> ' + todoText + '</li>');
+		// clear text input area
+		$(this).val('');
+	}
+});
+
+// Toggle the input area using the edit icon
+$('span').click(function() {
+	$("input[type='text']").fadeToggle();
+});
